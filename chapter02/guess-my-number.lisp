64f518b9bf12822a0b51d16f17606e9598cfb60e
Michael Schubart <michael@schubart.net>
2011-03-28 11:12:40 +0100

    start-over

--- chapter02/guess-my-number.lisp
+++ chapter02/guess-my-number.lisp
@@ -1,12 +1,8 @@
 ; You think of a number, the computer "guesses" it:
 ; In REPL:
 ; (load "guess-my-number.lisp")
-; (guess-my-number)
+; (start-over)
 ; (smaller) or (bigger) until your number is printed.
-
-(defparameter *small* 1)
-
-(defparameter *big* 100)
 
 (defun guess-my-number ()
   (ash (+ *small* *big*) -1))
@@ -17,4 +13,9 @@
 
 (defun bigger ()
   (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over ()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
   (guess-my-number))
